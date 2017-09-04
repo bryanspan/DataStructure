001546e808b35471bd421eaae21de783799e3ff1
shang <1511374059@qq.com>
2017-09-04 19:35:56 +0800

    update

--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -4,11 +4,11 @@
 #include <vector>
 using namespace std;
 
-//二维数组中的查找
+//1、二维数组中的查找
 bool find(int, vector<vector<int>>);
-//替换空格
+//2、替换空格
 char* replaceSpace(char *, int);
-//从尾到头打印链表
+//3、从尾到头打印链表
 struct ListNode {
 	int val;
 	struct ListNode *next;
@@ -16,6 +16,14 @@ struct ListNode {
 		val(x), next(NULL){}
 };
 vector<int> printListFromTailToHead(ListNode* head);
+//4、重建二叉树
+struct TreeNode{
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :val(x), left(NULL), right(NULL){}
+};
+TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> vin);
 
 
 
@@ -35,7 +43,7 @@ int main()
 
 
 
-//  题目描述 二维数组中的查找
+//  1、题目描述 二维数组中的查找
 /*  在一个二维数组中，每一行都按照从左到右递增的顺序排序，
     每一列都按照从上到下递增的顺序排序。请完成一个函数，
 	输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
@@ -74,7 +82,7 @@ bool find(int target, vector<vector<int>> array)
 }
 
 //
-//题目描述
+//2、题目描述
 //请实现一个函数，将一个字符串中的空格替换成“%20”。
 //例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
 /*思路：先计算出有多少空格 需要多少的空间，然后从后往前移动字符 这样移动的
@@ -109,9 +117,68 @@ char* replaceSpace(char *str, int length)
 	return pStr;	
 }
 
-/*题目描述
-输入一个链表，从尾到头打印链表每个节点的值。*/
+/*3、题目描述
+输入一个链表，从尾到头打印链表每个节点的值。
+Solution 1
+链表从尾到头输出，利用递归实现，不使用库函数直接printf输出，使用递归比较好
+Solution 2
+用库函数，每次扫描一个节点，将该结点数据存入vector中，如果该节点有下一个结点，将下一个结点
+数据直接插入vector最前面，直至遍历完，
+Solution 3
+或者，直接加在最后最后调用reverse*/
+
+//Solution 1 
 vector<int> printListFromTailToHead(ListNode* head)
+{
+	vector<int> listVec;
+	while (head != NULL)
+	{
+		listVec.insert(listVec.begin(), head->val);
+		if (head->next != NULL)
+		{
+			vector<int> tempVec = printListFromTailToHead(head->next);
+			if (tempVec.size() > 0)
+				listVec.insert(listVec.begin(), tempVec.begin(), tempVec.end());
+		}
+	}
+	return listVec;
+}
+
+//Solution 2  time:2ms space:620k
+vector<int> printListFromTailToHead(ListNode* head)
+{
+	vector<int> listVec;
+	while (head != NULL)
+	{
+		listVec.insert(listVec.begin(), head->val);
+		head = head->next;
+	}
+	return listVec;
+}
+
+
+//Solution 3  time:1ms  space:500k
+vector<int> printListFromTailToHead(ListNode* head)
+{
+	vector<int> listNodeVec;
+	while (head != NULL)
+	{
+		listNodeVec.push_back(head->val);
+		head = head -> next;
+	}
+	return vector<int>(listNodeVec.rbegin, listNodeVec.rend);
+
+}
+
+/*
+4、重建二叉树
+题目描述：
+输入某二叉树的前序遍历和中序遍历的结果，
+请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+例如输入前序遍历序列{1,2,4,7,3,5,6,8}
+和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+*/
+TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> vin)
 {
 
 }
